9befbdbf24ed3da6da52eb92de21efc2b8983144
Marcus <kumorig@gmail.com>
2015-10-29 17:40:06 +0900

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  twitter_config: {
+    consumer_key         : '...'
+    , consumer_secret    : '...'
+    , access_token       : '...'
+    , access_token_secret: '...'
+  }
+};
